bc2b71b56dc6d3cc8fc01753f3583d837ae8492a
nhannt56 <nhannt56@unifiedpost.com>
2021-06-12 03:05:26 +0700

    First Commit

---
+++ AutomationFrameWorkWeb/PageObject/WebAbstractPage.cs
@@ -0,0 +1,127 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationFrameWorkWeb.PageObject
+{
+  public class WebAbstractPage
+    {
+        By by;
+        Action action;
+        IWebElement element;
+        TimeSpan shortTimeOut = new TimeSpan(5);
+        TimeSpan longTimeOut = new TimeSpan(60);
+        IWebDriver driver;
+        List<IWebElement> elements;
+        WebDriverWait waitExplicit;
+        public WebAbstractPage(string browser)
+        {
+            this.driver = startWebBrowser(browser);
+            waitExplicit = new WebDriverWait(driver,longTimeOut);
+        
+        }
+
+        #region Open URL
+        public void openUrl(string ulrValue) { driver.Navigate().GoToUrl(ulrValue); }
+        #endregion
+
+        #region Maximum Browser
+        public void maximumBrowser() { driver.Manage().Window.Maximize(); }
+        #endregion
+
+        #region Back to page
+        public void backToPage() { driver.Navigate().Back(); }
+        #endregion
+
+        #region Accept Alert
+        public void acceptAlert() { driver.SwitchTo().Alert().Accept(); }
+        #endregion
+
+        #region Cancle Alert
+        public void cancleAlert() { driver.SwitchTo().Alert().Dismiss(); }
+        #endregion
+
+        #region Wait to Alert Presence
+        public void waitToAlertPresence() { waitExplicit.Until (ExpectedConditions.AlertIsPresent()); }
+        #endregion
+
+        #region Sendkey To Alert
+        public void sendkeyToAlert(string value) { driver.SwitchTo().Alert().SendKeys(value); }
+        #endregion
+
+        #region Get Text Alert
+        //      public void getTextAlert() { driver.SwitchTo().Alert().GetType(); }
+        #endregion
+
+        #region Click to element
+        public void clickToElement(string type, string locator)
+        {
+            element = driver.FindElement(elementAttribute(type,locator));
+            element.Click();
+        }
+        #endregion
+
+        #region Sendkey to element
+        public void sendKeyToElement(string type, string locator, string value)
+        {
+            element = driver.FindElement(elementAttribute(type, locator));
+            waitToElementPresent(type, locator);
+            element.Clear();
+            element.SendKeys(value);
+        }
+        #endregion
+
+        #region Wait to Element present
+        public void waitToElementPresent(string type, string locator)
+        {
+            by = elementAttribute(type, locator);
+            waitExplicit.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(by));
+        }
+        #endregion
+
+        By elementAttribute(string type,string locator)
+        {
+
+            switch (type.ToUpper())
+            {
+                case "CSS":
+                    by = By.CssSelector(locator);
+                    return by;
+                    break;
+
+                case "XPATH":
+                    by = By.XPath(locator);
+                    return by;
+                    break;
+
+                default:
+                    by = By.XPath(locator);
+                    return by;
+                    break;
+            }
+           
+        }
+
+        public static IWebDriver startWebBrowser(string browser)
+        {
+            switch (browser)
+            {
+                case "CHROME":
+                    return new ChromeDriver();
+                    break;
+
+                case "FIREFOX":
+                    return new FirefoxDriver();
+
+                default:
+                    return new ChromeDriver();
+                    break;
+            }
+        }
+        
+    }
+}
---
+++ AutomationFrameWorkWeb/Test/BaseTest.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+
+namespace AutomationFrameWorkWeb.Test
+{
+  
+    
+  public class BaseTest : IDisposable
+    {
+        IWebDriver driver;
+       
+        public BaseTest()
+        {
+            driver.Manage().Window.Maximize();
+
+        }
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+   
+    }
+
+    class Tesfigture : IClassFixture<BaseTest>
+    {
+        public Tesfigture(BaseTest data)
+        {
+
+        }
+    }
+
+   
+}
