464fec9a1d8bf71b050282fee830d6ed7eb30e0e
nhannt56 <nhannt56@unifiedpost.com>
2021-06-12 12:42:04 +0700

    New Function

--- AutomationFrameWorkWeb/PageObject/WebAbstractPage.cs
+++ AutomationFrameWorkWeb/PageObject/WebAbstractPage.cs
@@ -3,6 +3,7 @@
     using OpenQA.Selenium;
     using OpenQA.Selenium.Chrome;
     using OpenQA.Selenium.Firefox;
+    using OpenQA.Selenium.Interactions;
     using OpenQA.Selenium.Support.UI;
     using System;
     using System.Collections.Generic;
@@ -13,50 +14,19 @@
     /// </summary>
     public class WebAbstractPage
     {
-        /// <summary>
-        /// Defines the select.
-        /// </summary>
         SelectElement select;
-
-        /// <summary>
-        /// Defines the by.
-        /// </summary>
         By by;
-
-        /// <summary>
-        /// Defines the action.
-        /// </summary>
-        Action action;
-
-        /// <summary>
-        /// Defines the element.
-        /// </summary>
+        Actions actions;
         IWebElement element;
-
-        /// <summary>
-        /// Defines the shortTimeOut.
-        /// </summary>
         TimeSpan shortTimeOut = new TimeSpan(5);
-
-        /// <summary>
-        /// Defines the longTimeOut.
-        /// </summary>
         TimeSpan longTimeOut = new TimeSpan(60);
-
-        /// <summary>
-        /// Defines the driver.
-        /// </summary>
         IWebDriver driver;
-
-        /// <summary>
-        /// Defines the elements.
-        /// </summary>
         List<IWebElement> elements;
-
-        /// <summary>
-        /// Defines the waitExplicit.
-        /// </summary>
         WebDriverWait waitExplicit;
+        IJavaScriptExecutor js;
+
+
+
 
         /// <summary>
         /// Initializes a new instance of the <see cref="WebAbstractPage"/> class.
@@ -64,8 +34,11 @@
         /// <param name="browser">The browser<see cref="string"/>.</param>
         public WebAbstractPage(string browser)
         {
+            js = (IJavaScriptExecutor)driver;
             this.driver = startWebBrowser(browser);
             waitExplicit = new WebDriverWait(driver, longTimeOut);
+            actions = new Actions(driver);
+            
         }
 
         /// <summary>
@@ -262,6 +235,165 @@
         {
             element = driver.FindElement(elementAttribute(type, locator));
             return element.Text;
+        }
+        #endregion
+
+        #region Get color element
+        public string getColorElement(string type, string locator, string color)
+        {
+            element = driver.FindElement(elementAttribute(type, locator));
+            return element.GetCssValue(color);
+        }
+        #endregion
+
+        #region Check radio button
+        public void checkRadioButton(string type, string locator)
+        {
+            element.FindElement(elementAttribute(type, locator));
+            if (!element.Selected)
+            {
+                element.Click();
+            }
+        }
+        #endregion
+
+        #region Uncheck radio button
+        public void unCheckRadioButton(string type, string locator)
+        {
+            element.FindElement(elementAttribute(type, locator));
+            if (!element.Selected)
+            {
+                element.Click();
+            }
+        }
+        #endregion
+
+        #region Element is displayed
+        public bool isElementDisplayed(string type, string locator)
+        {
+            try
+            {
+                // Handle element exists in DOM, display or not
+                element = driver.FindElement(elementAttribute(type, locator));
+                return element.Displayed;
+            }
+            catch (NoSuchElementException e)
+            {
+
+                return false;
+            }
+        }
+        #endregion
+
+        #region Switch to iframe
+        public void switchToIframe(string type, string locator)
+        {
+            element = driver.FindElement(elementAttribute(type, locator));
+            driver.SwitchTo().Frame(element);
+        }
+        #endregion
+
+        #region Double click to element
+        public void doubleClickToElement(string type, string locator)
+        {
+            element = driver.FindElement(elementAttribute(type, locator));
+            actions.DoubleClick(element);
+        }
+        #endregion
+
+        #region Send keyboard to element
+        public void sendKeyboardToElement(string type, string location, string key)
+        {
+            element = driver.FindElement(elementAttribute(type, location));
+            actions.SendKeys(element, key).Perform();
+        }
+        #endregion
+
+        #region Verify Image loaded
+        public bool verifyImageLoaded(string type, string locator)
+        {
+            bool status;
+            element = driver.FindElement(elementAttribute(type, locator));
+            status = (bool)js.ExecuteScript("return argument[0].complete && typeof arguments[0].naturalWidth != \"undefined\" && argument [0].naturalWidth>0", element);
+            if (status)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Wait to Element Visible
+        public void waitToElementVisible(string type, string locator)
+        {
+            by = elementAttribute(type, locator);
+            waitExplicit.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(by));
+        }
+        #endregion
+
+        #region Wait to Element Clickable
+        public void waittoElementClickable(string type, string locator)
+        {
+            by = elementAttribute(type, locator);
+            waitExplicit.Until(ExpectedConditions.ElementToBeClickable(by));
+        }
+        #endregion
+
+        #region Scroll to element
+        public void scrollToElement(string type, string locator)
+        {
+            element = driver.FindElement(elementAttribute(type, locator));
+            actions.MoveToElement(element);
+            actions.Perform();
+        }
+        #endregion
+
+        #region JS ScrollToElement
+        public void jsScrollToElement(string type, string locator)
+        {
+            element = driver.FindElement(elementAttribute(type, locator));
+            js.ExecuteScript("arguments[0].scrollIntoView(true);", element);
+        }
+        #endregion
+
+        #region JS Click On
+        public void jsClickOn(string type, string locator)
+        {
+            element = driver.FindElement(elementAttribute(type, locator));
+            js.ExecuteScript("argument[0].click()", element);
+        }
+        #endregion
+
+        #region JS run on LOCATOR
+        public void jsRunOnLocator(string javaScripToRunOn)
+        {
+            js.ExecuteScript(javaScripToRunOn);
+        }
+        #endregion
+
+        #region Highligh Element
+        public void highlighElement(string type, string locator)
+        {
+            element = driver.FindElement(elementAttribute(type, locator));
+            for(int i = 0; i<10; i++)
+            {
+                js.ExecuteScript("arguments[0].setAttribute('style', arguments[1]);", element,
+                    "color: red; border: 2px solid red;");
+                js.ExecuteScript("arguments[0].setAttribute('style', arguments[1]);", element, "");
+            }
+        }
+        #endregion
+
+        #region Close
+        public void Close()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+            }
         }
         #endregion
 
